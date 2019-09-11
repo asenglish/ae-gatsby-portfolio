58346dd22f8cfec1f2b2837ba8d611914dc4b761
Andrew English <andrewenglish@Andrews-MacBook-Pro.local>
2019-09-10 21:17:41 -0700

    working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import photos from "../lib/photos.json"
 
-export default () => <div>Hello world!</div>
+export default () => {
+    let rendered = [];
+    photos.photos.forEach((photo) => {
+        rendered.push(<img src={photo.url} style={{width: 100}, {height: 100}}/>)
+    })
+    return rendered;
+}
